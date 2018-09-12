cf69451506b2c496d2ddd15a200d5ecdbe7c26ff
Papop Ruengsupapichat <>
2018-09-12 18:22:44 +0700

    [ UPDATE ] config project

---
+++ src/components/index/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class SectionHero extends Component {
+    render() {
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-8'>
+                        <h1>Hello World <br/> Mock Up</h1>
+                        <button type="button" class="btn btn-warning">JOIN</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SectionHero
