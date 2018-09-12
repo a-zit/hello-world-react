5e35aad16c5312324f1cbfc8b54d08444a86b8d9
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-12 18:43:50 +0700

    [ADD] HeroSection file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-
+import Index from './component/Index'
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Index/>
     );
   }
 }
---
+++ src/component/Index/index.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+import HeroSection from './HeroSection'
+export default class index extends Component {
+  render() {
+    return (
+      <HeroSection/>
+    )
+  }
+}
