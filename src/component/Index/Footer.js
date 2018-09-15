fc989cb564d434653c8086002b938539fc058204
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-15 19:13:36 +0700

    [ADD] Footer.js

--- src/component/Index/Footer.js
+++ src/component/Index/Footer.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer=()=>(
-    <div></div>
+    <div className='d-flex justify-content-between align-items-center' style={{height:'50px',width:'auto',backgroundColor:'gray'}}>
+    <div>
+        <p>This is footer</p>
+    </div>
+    </div>
 );
 
 export default Footer;
