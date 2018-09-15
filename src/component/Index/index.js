a35f00179f74f538bdb8a80c6b7bd8e2b8134ae0
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-15 18:20:16 +0700

    [REMOVE] index and hero section

--- src/component/Index/index.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from 'react'
-import HeroSection from './HeroSection'
-export default class index extends Component {
-  render() {
-    return (
-      <HeroSection/>
-    )
-  }
-}
