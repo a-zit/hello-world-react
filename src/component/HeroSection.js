eba1b02f01fc5a6723c103054abeefa1f4467068
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-15 18:19:49 +0700

    [UPDATE] format with lint

---
+++ src/component/HeroSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HeroSection = ()=>(
+    <div className='container mt-5'>
+        <div>Hero Section</div>
+        <button type="button" class="btn btn-warning">Join</button>
+    </div>
+);
+
+export default HeroSection;
