2e9e3ca69dcb1022bb9584070d2ad5053424324d
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-15 18:36:02 +0700

    [UPDATE] add Footer in page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import Navbar from '../src/component/Navbar';
+import Navbar from '../src/component/Navbar.js';
+import Topic from '../src/component/TopicSection.js';
 class App extends Component {
   render() {
     return (
+      <div>
       <Navbar />
+      <Topic />
+      </div>
     );
   }
 }
