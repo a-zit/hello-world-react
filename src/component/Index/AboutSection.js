7ae22c0012535d22ff1958bc5eebf2991575d82c
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-15 19:34:39 +0700

    [UPDATE] text and style

--- src/component/Index/AboutSection.js
+++ src/component/Index/AboutSection.js
@@ -1,16 +1,17 @@
 import React from 'react';
-const text = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-irure dolor in reprehenderit in voluptate velit esse cillum 
-dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-cupidatat non proident, sunt in culpa qui officia deserunt 
-mollit anim id est laborum.`;
 
 const AboutSection = ()=>(
-<div>
-    <p>{text}</p>
+<div className='Container py-4 mx-5' style={{height:'200px'}}>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco
+laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+irure dolor in reprehenderit in voluptate velit esse cillum
+dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+cupidatat non proident, sunt in culpa qui officia deserunt
+mollit anim id est laborum.
+    </p>
 </div>
 );
 
