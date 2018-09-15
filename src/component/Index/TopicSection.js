dc2dbc912e3edb08a901053ddfa2ae4f5234c306
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-15 19:01:08 +0700

    [UPDATE] move to index and add css style

---
+++ src/component/Index/TopicSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TopicSection = ()=>(
+    <div style={{backgroundColor:'yellow',padding:0,margin:0}}>
+    <div className='container pt-5 pb-5'>
+    <div className=''>
+
+        <h1>Topic Section</h1>
+        <button type="button" class="btn btn-warning">Join</button>
+    </div>
+    </div>
+    </div>
+);
+
+export default TopicSection;
