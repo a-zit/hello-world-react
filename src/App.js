247462a2397fb3cf4a4e22a2dcb2e1f958e8b73c
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-15 19:14:20 +0700

    [UPDATE] add AboutSection and Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './component/Index/Navbar.js';
 import Topic from './component/Index/TopicSection.js';
+import AboutSection from './component/Index/AboutSection.js';
+import Footer from './component/Index/Footer.js';
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
         <Topic />
+        <AboutSection />
+        <Footer />
       </div>
     );
   }
