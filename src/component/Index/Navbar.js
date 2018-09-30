77cfde8aa70a713394330c275bef2d75b1da21ff
Joknoi <panupong.pj15@mail.kmutt.ac.th>
2018-10-01 00:09:24 +0700

    [UPDATE] Navbar

--- src/component/Index/Navbar.js
+++ src/component/Index/Navbar.js
@@ -1,26 +1,18 @@
 import React from 'react';
 
 const Navbar = () => (
-    <div className='container-fluid'>
-        <div className='navbar navbar-expand-lg navbar-light bg-light'>
-            <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarTogglerDemo01' aria-controls='navbarTogglerDemo01' aria-expanded='false' aria-label='Toggle navigation'>
-                <span className='navbar-toggler-icon'></span>
-            </button>
-            <div className='' id='navbarTogglerDemo01'>
-                <a className='navbar-brand mr-auto mt-2 mt-lg-0'></a>
-                <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
-                    <li className='nav-item active'>
-                        <a className='nav-link' href='#'>Home<span className='sr-only'>(current)</span></a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='#'Link>product</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link ' href='#'>contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        </div>
-        );
+    <div className='navbar navbar-expand navbar-light bg-light'>
+        <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+            <li className='nav-item active'>
+                <a className='nav-link' href='#'>Home<span className='sr-only'>(current)</span></a>
+            </li>
+            <li className='nav-item'>
+                <a className='nav-link' href='#' Link>product</a>
+            </li>
+            <li className='nav-item'>
+                <a className='nav-link ' href='#'>contact</a>
+            </li>
+        </ul>
+    </div>
+);
 export default Navbar;
